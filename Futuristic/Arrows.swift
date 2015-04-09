ce1b5fae295ace854b88a3f1e3ab5ddaaa3f7d7a
Matt Donnelly <mattdonnelly@me.com>
2015-04-09 19:36:20 +0100

    Use tuples properly for inputs and improved comments

--- Futuristic/Arrows.swift
+++ Futuristic/Arrows.swift
@@ -15,55 +15,55 @@ infix operator *** { associativity left  precedence 200 }
 infix operator &&& { associativity left  precedence 200 }
 
 /** 
- *  A -> [f] -> B
+ *  A ──▶ [f] ──▶ B
  */
 func |> <A, B>(left: A, right: A -> B) -> B {
     return right(left)
 }
 
 /**
- *  B <- [f] <- A
+ *  B ◀── [f] ◀── A
  */
 func <| <A, B>(left: A -> B, right: A) -> B {
     return left(right)
 }
 
 /**
- *  A -> [f] -> B -> [g] -> C
+ *  A ──▶ [f] ──▶ B ──▶ [g] ──▶ C
  */
 func >>> <A, B, C>(f: Future<A> -> Future<B>, g: Future<B> -> Future<C>) -> (Future<A> -> Future<C>) {
     return { g(f($0)) }
 }
 
 /**
- *  A -> [f] -> C
- *  B -> [g] -> D
+ *  A ──▶ [f] ──▶ C
+ *  B ──▶ [g] ──▶ D
  */
-func *** <A, B, C, D>(f: Future<A> -> Future<B>, g: Future<C> -> Future<D>) -> ((Future<A>, Future<C>) -> (Future<B>, Future<D>)) {
-    return { (f($0), g($1)) }
+func *** <A, B, C, D>(f: Future<A> -> Future<C>, g: Future<B> -> Future<D>) -> ((Future<A>, Future<B>)) -> (Future<C>, Future<D>) {
+    return { (f($0.0), g($0.1)) }
 }
 
 /**
- *  A ---> [f] -> B
- *     |
- *      -> [g] -> C
+ *      ┌──▶ [f] ──▶ B
+ *  A ──┤
+ *      └──▶ [g] ──▶ C
  */
 func &&& <A, B, C>(f: Future<A> -> Future<B>, g: Future<A> -> Future<C>) -> (Future<A> -> (Future<B>, Future<C>)) {
-    return { (f($0), g($0)) }
+    return { (f($0.0), g($0.0)) }
 }
 
 /**
- *  A -> [f] -> C
- *  B --------> B
+ *  A ──▶ [f] ──▶ C
+ *  B ──────────▶ B
  */
-func first<A, B, C>(f: Future<A> -> Future<C>) -> ((Future<A>, Future<B>) -> (Future<C>, Future<B>)) {
-    return { (f($0), $1) }
+func first<A, B, C>(f: Future<A> -> Future<C>) -> ((Future<A>, Future<B>)) -> (Future<C>, Future<B>) {
+    return { (f($0.0), $0.1) }
 }
 
 /**
- *  A --------> A
- *  B -> [f] -> C
+ *  A ──────────▶ A
+ *  B ──▶ [f] ──▶ C
  */
-func second<A, B, C>(f: Future<B> -> Future<C>) -> ((Future<A>, Future<B>) -> (Future<A>, Future<C>)) {
-    return { ($0, f($1)) }
+func second<A, B, C>(f: Future<B> -> Future<C>) -> ((Future<A>, Future<B>)) -> (Future<A>, Future<C>) {
+    return { ($0.0, f($0.1)) }
 }
